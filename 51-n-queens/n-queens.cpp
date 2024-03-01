463c9104fb2e4cb9bce87c382511de7db4f49966
Abhinav Gangwar <abhinav1203gangwar@gmail.com>
2024-03-01 20:37:06 +0530

    Time: 40 ms (15.60%) | Memory: 49.1 MB (11.06%) - LeetSync

---
+++ 51-n-queens/n-queens.cpp
@@ -0,0 +1,62 @@
+class Solution {
+    private:
+    
+bool helper(vector<string> grid,int curr_column, int curr_row,int size){
+    // up
+    for(int i=1;(curr_row - i ) >= 0;i++){
+        if(grid[curr_row - i][curr_column] == 'Q')return false;
+    }
+    // down
+    for(int i=1;(curr_row + i ) < size ;i++){
+        if(grid[curr_row + i][curr_column] == 'Q')return false;
+    }
+    // left
+    for(int i=1;(curr_column - i ) >= 0;i++){
+        if(grid[curr_row][curr_column-i] == 'Q')return false;
+    }
+    // right
+    for(int i=1;(curr_column + i ) < size;i++){
+        if(grid[curr_row][curr_column+i] == 'Q')return false;
+    }
+    // diagonals
+    // up
+    for(int i=1;(curr_row - i ) >= 0 && (curr_column - i) >= 0;i++){
+        if(grid[curr_row - i][curr_column - i] == 'Q')return false;
+    }
+    // down
+    for(int i=1;(curr_row + i ) < size && (curr_column + i) < size;i++){
+        if(grid[curr_row + i][curr_column +i] == 'Q')return false;
+    }
+    // second d_up
+    for(int i=1;(curr_row - i ) >= 0 && (curr_column + i) < size;i++){
+        if(grid[curr_row - i][curr_column +i] == 'Q')return false;
+    }
+    // second d_down
+    for(int i=1;(curr_row + i ) < size && (curr_column - i) >= 0;i++){
+        if(grid[curr_row + i][curr_column -i] == 'Q')return false;
+    }
+    return true;
+}
+
+void foo(vector<string> &grid,int column,int row,int size,vector<vector<string>> &ans){
+    if(row >= size){
+        ans.push_back(grid);
+        return;
+    }
+    for(int x = 0;x<size;x++){
+        if(helper(grid,column+x,row,size)){
+            grid[row][x] = 'Q';
+            foo(grid,0,row+1,size,ans);
+            grid[row][x] = '.';
+        }
+    }
+    return;
+}
+public:
+    vector<vector<string>> solveNQueens(int n) {
+         vector<string> grid(n,string(n,'.'));
+        vector<vector<string>> ans;
+        foo(grid,0,0,n,ans);
+        return ans;
+    }
+};
