ff992c4fcbc689aeeb3e977f8328f14cd6dbfeac
Abhinav Gangwar <abhinav1203gangwar@gmail.com>
2024-03-21 22:14:39 +0530

    Time: 35 ms (83.46%) | Memory: 17.1 MB (69.86%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -7,26 +7,58 @@
  * };
  */
 class Solution {
+    private:
+    int len_(ListNode* head){
+        int len = 0;
+        ListNode* temp =  head;
+        while(temp){
+            len++;
+            temp = temp->next;
+        }
+        return len;
+    }
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        vector<ListNode*> lista;
-        vector<ListNode*> listb;
+        // vector<ListNode*> lista;
+        // vector<ListNode*> listb;
 
+        // ListNode* temp = headA;
+        // while(temp){
+        //     lista.push_back(temp);
+        //     temp = temp->next;
+        // }
+        // temp = headB;
+        // while(temp){
+        //     listb.push_back(temp);
+        //     temp = temp->next;
+        // }
+        // for(int i=0;i<lista.size();i++){
+        //     for(int j=0;j<listb.size();j++){
+        //         if(lista[i] == listb[j])return lista[i];
+        //     }
+        // }
+        // return NULL;
+
+
+        if(headA == NULL or headB == NULL)return NULL;
         ListNode* temp = headA;
-        while(temp){
-            lista.push_back(temp);
-            temp = temp->next;
-        }
-        temp = headB;
-        while(temp){
-            listb.push_back(temp);
-            temp = temp->next;
-        }
-        for(int i=0;i<lista.size();i++){
-            for(int j=0;j<listb.size();j++){
-                if(lista[i] == listb[j])return lista[i];
+        ListNode* temp_ = headB;
+        int len = len_(headA),len1 = len_(headB);
+        int diff = abs(len - len1);
+        if(len > len1){
+            for(int i=0;i<diff;i++){
+                temp = temp->next;
             }
         }
-        return NULL;
+        else{
+            for(int i=0;i<diff;i++){
+                temp_ = temp_->next;
+            }
+        }
+        while (temp != temp_){
+            temp = temp->next;
+            temp_ = temp_->next;
+        }
+        return temp;
     }
 };
